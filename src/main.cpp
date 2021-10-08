19b9ea4b5d2493863d3c27fea2e685e6d0020b02
Lilian Kempees <45563232+YolaLilian@users.noreply.github.com>
2021-10-08 17:03:21 +0200

    Add DHT sensor functionality (serial monitor)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
-#include <Arduino.h>
+// Includes
+
+// DHT
+#include <DHT.h>
+
+// Defines
+
+// DHT
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+// Initialize
+
+// DHT
+DHT dht(DHTPIN, DHTTYPE);
+
+// Variables
+
+// DHT variables
+float t = 0.0;    // Temperature
+float h = 0.0;    // Humidity
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // Start Serial (for debugging purpose)
+  Serial.begin(9600);
+  Serial.println("Starting setup!");
+
+  // Starting sensors
+  dht.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Pause between measurements
+  delay(5000);
+
+  // Take readings from sensor
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+
+  // Check if empty or failed reading
+  if ( isnan(t) ) {
+    Serial.println("Failed to read temperature!");
+  }
+  if ( isnan(h) ) {
+    Serial.println("Failed to read humidity!");
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println("°C");
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+
+// Serial.print("Humidity: "); Serial.println(dht.readHumidity());
+// Serial.print("Temperature: "); Serial.println(dht.readTemperature());
+
 }
